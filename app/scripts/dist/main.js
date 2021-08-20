27dd058452033fc1a9ab70515a206036d41c3335
Akriti Yadav <94akritiyadav@gmail.com>
2021-08-20 12:06:21 +0530

    Chatter Box ES6 using BABEL

---
+++ app/scripts/dist/main.js
@@ -0,0 +1,1 @@
+console.error("Error: Plugin/Preset files are not allowed to export objects, only functions. In C:\\Users\\Akriti Yadav\\Desktop\\front-end-dev-book\\chattrbox\\node_modules\\babel-preset-es2015\\lib\\index.js while parsing file: C:\\Users\\Akriti Yadav\\Desktop\\front-end-dev-book\\chattrbox\\app\\scripts\\src\\main.js");
